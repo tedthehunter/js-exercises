e9b4efe48dfba51d6f927c53542c169716f39194
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-01 19:16:10 -0700

    Comment out 1st exercise tests and label
    separate tests will make file more readable

--- array methods/script.js
+++ array methods/script.js
@@ -14,6 +14,10 @@ const camelize = (string) => {
     return joinedArray;
 }
 
-console.log(camelize("background-color") == 'backgroundColor');
-console.log(camelize("list-style-image") == 'listStyleImage');
-console.log(camelize("-webkit-transition") == 'WebkitTransition');
+
+
+/* ***TEST SUITE*** */
+
+//console.log(camelize("background-color") == 'backgroundColor');
+//console.log(camelize("list-style-image") == 'listStyleImage');
+//console.log(camelize("-webkit-transition") == 'WebkitTransition');

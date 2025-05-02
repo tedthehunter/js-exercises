abc631a4623b479ba27ac7ff860b45338a70fa39
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-01 19:56:28 -0700

    name function and tests for exercise 3

--- array methods/script.js
+++ array methods/script.js
@@ -14,23 +14,28 @@ const camelize = (string) => {
     return joinedArray;
 }
 
-//FUNCTION - accepts inputArray, lowerNum, HigherNum
-    //FILTER input array 
 const filterRange = (inputArray, lowerNum, higherNum) => {
-    return inputArray.filter(function(item) {
-        if (item >= lowerNum && item <= higherNum) {
-            return item;
-        }
-    });
+    return inputArray.filter(item => (item >= lowerNum && item <= higherNum));
 }
 
+const filterRangeInPlace = () => {
+
+}
 
 /* ***TEST SUITE*** */
 
 //EXERCISE 1
-//console.log(camelize("background-color") == 'backgroundColor');
-//console.log(camelize("list-style-image") == 'listStyleImage');
-//console.log(camelize("-webkit-transition") == 'WebkitTransition');
+console.log(camelize("background-color") == 'backgroundColor');
+console.log(camelize("list-style-image") == 'listStyleImage');
+console.log(camelize("-webkit-transition") == 'WebkitTransition');
 
 //EXERCISE 2
-//
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+console.log( filtered ); // 3,1 (matching values)
+console.log( arr ); // 5,3,8,1 (not modified)
+
+//EXERCISE 3
+let arr2 = [5, 3, 8, 1];
+filterRangeInPlace(arr2, 1, 4); // removed the numbers except from 1 to 4
+alert( arr2 ); // [3, 1]
